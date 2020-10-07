29d8499cdc8d3d27a71dc6ee177fdff47a732679
Santiago Pavon <santiago@dit.upm.es>
2020-10-07 13:00:44 +0200

    Step 4.4: Authorization.

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const quizController = require('../controllers/quiz');
+const sessionController = require("../controllers/session");
 
 
 // Autoload for routes using :quizId
@@ -9,13 +10,26 @@ router.param('quizId', quizController.load);
 
 
 // Routes for the resource /quizzes
-router.get('/',                    quizController.index);
-router.get('/:quizId(\\d+)',       quizController.show);
-router.get('/new',                 quizController.new);
-router.post('/',                   quizController.create);
-router.get('/:quizId(\\d+)/edit',  quizController.edit);
-router.put('/:quizId(\\d+)',       quizController.update);
-router.delete('/:quizId(\\d+)',    quizController.destroy);
+router.get('/',
+  quizController.index);
+router.get('/:quizId(\\d+)',
+  sessionController.loginRequired,
+  quizController.show);
+router.get('/new',
+  sessionController.loginRequired,
+  quizController.new);
+router.post('/',
+  sessionController.loginRequired,
+  quizController.create);
+router.get('/:quizId(\\d+)/edit',
+  sessionController.loginRequired,
+  quizController.edit);
+router.put('/:quizId(\\d+)',
+  sessionController.loginRequired,
+  quizController.update);
+router.delete('/:quizId(\\d+)',
+  sessionController.loginRequired,
+  quizController.destroy);
 
 router.get('/:quizId(\\d+)/play',  quizController.play);
 router.get('/:quizId(\\d+)/check', quizController.check);
--- routes/users.js
+++ routes/users.js
@@ -2,20 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 const userController = require('../controllers/user');
-const quizController = require("../controllers/quiz");
+const sessionController = require("../controllers/session");
 
 
 // Autoload for routes using :userId
 router.param('userId', userController.load);
 
 // Routes for the resource /users
-router.get('/',                   userController.index);
-router.get('/:userId(\\d+)',      userController.show);
-router.get('/new',                userController.new);
-router.post('/',                  userController.create);
-router.get('/:userId(\\d+)/edit', userController.isLocalRequired, userController.edit);
-router.put('/:userId(\\d+)',      userController.isLocalRequired, userController.update);
-router.delete('/:userId(\\d+)',   userController.destroy);
+router.get('/',
+  sessionController.loginRequired,
+  userController.index);
+router.get('/:userId(\\d+)',
+  sessionController.loginRequired,
+  userController.show);
+router.get('/new',
+  userController.new);
+router.post('/',
+  userController.create);
+router.get('/:userId(\\d+)/edit',
+  sessionController.loginRequired,
+  userController.isLocalRequired,
+  sessionController.adminOrMyselfRequired,
+  userController.edit);
+router.put('/:userId(\\d+)',
+  sessionController.loginRequired,
+  userController.isLocalRequired,
+  sessionController.adminOrMyselfRequired,
+  userController.update);
+router.delete('/:userId(\\d+)',
+  sessionController.loginRequired,
+  sessionController.adminOrMyselfRequired,
+  userController.destroy);
 
 
 module.exports = router;
