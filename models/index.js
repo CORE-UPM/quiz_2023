ienamorado/quiz_2021

Step 6.1: Quiz with Attachment.

models/index.js
@@ -1,11 +1,17 @@
 const sequelize = require('./sequelize');
 const Quiz = require('./quiz');
 const User = require('./user');
+const Attachment = require('./attachment');
 const Session = require('./session');
 
 // Relation 1-to-N between User and Quiz:
 User.hasMany(Quiz, {as: 'quizzes', foreignKey: 'authorId'});
 Quiz.belongsTo(User, {as: 'author', foreignKey: 'authorId'});
+
+
+// Relation 1-to-1 between Quiz and Attachment
+Attachment.hasOne(Quiz, {as: 'quiz', foreignKey: 'attachmentId'});
+Quiz.belongsTo(Attachment, {as: 'attachment', foreignKey: 'attachmentId'});
 
 
 module.exports = sequelize;

CORE-UPM/quiz_2023

models/index.js
@@ -11,10 +11,16 @@ const sequelize = new Sequelize(url);
 const Session = require('./session')(sequelize);
 const Quiz = require('./quiz')(sequelize);
 const User = require('./user')(sequelize);
+const Attachment = require('./attachment')(sequelize);
 
 // Relation 1-to-N between User and Quiz:
 User.hasMany(Quiz, {as: 'quizzes', foreignKey: 'authorId'});
 Quiz.belongsTo(User, {as: 'author', foreignKey: 'authorId'});
+
+
+// Relation 1-to-1 between Quiz and Attachment
+Quiz.hasOne(Attachment, {as: 'attachment', foreignKey: 'quizId'});
+Attachment.belongsTo(Quiz, {as: 'quiz', foreignKey: 'quizId'});
 
 
 module.exports = sequelize;