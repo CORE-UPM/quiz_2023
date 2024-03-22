ienamorado/quiz_2021

Step 6.2: User with Photo.

models/index.js
@@ -14,4 +14,9 @@ Attachment.hasOne(Quiz, {as: 'quiz', foreignKey: 'attachmentId'});
 Quiz.belongsTo(Attachment, {as: 'attachment', foreignKey: 'attachmentId'});
 
 
+// Relation 1-to-1 between User and Attachment
+User.belongsTo(Attachment, {as: "photo", foreignKey: 'photoId'});
+Attachment.hasOne(User, {as: 'user', foreignKey: 'photoId'});
+
+
 module.exports = sequelize;

CORE-UPM/quiz_2023

models/index.js
@@ -23,4 +23,9 @@ Quiz.hasOne(Attachment, {as: 'attachment', foreignKey: 'quizId'});
 Attachment.belongsTo(Quiz, {as: 'quiz', foreignKey: 'quizId'});
 
 
+// Relation 1-to-1 between User and Attachment
+User.hasOne(Attachment, {as: "photo", foreignKey: 'userId'});
+Attachment.belongsTo(User, {as: 'user', foreignKey: 'userId'});
+
+
 module.exports = sequelize;